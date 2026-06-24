324d890172a44e4411372187828b7918493857d5
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-06-24 20:20:20 +0900

    Initial Commit

---
+++ DailyTudum/DailyTudumApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTudumApp.swift
+//  DailyTudum
+//
+//  Created by joe on 6/24/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyTudumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
